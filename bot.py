2da37a10dece227298613c8dc2afca6f30c66554
Rohit Ner <rohitner1@gmail.com>
2020-08-19 04:44:44 +0530

    add files

---
+++ bot.py
@@ -0,0 +1,88 @@
+import random
+import os
+from flask import Flask, request
+from pymessenger.bot import Bot
+from generate import generateVideoClip
+from threading import Thread
+
+app = Flask(__name__)
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
+bot = Bot(ACCESS_TOKEN)
+
+
+# We will receive messages that Facebook sends our bot at this endpoint
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    # if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    # Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        # response_sent_text = get_message()
+                        url = message['message'].get('text')
+                        print('text')
+                        print(url)
+                        videoThread = Thread(target=pushVideoClip, args=(recipient_id, url,))
+                        videoThread.daemon = True
+                        videoThread.start()
+                        # send_message(recipient_id, 'Sorry, your request could not be processed :/')
+                        # send_video(recipient_id, '/home/rohitner/newzery/stock.mp4')
+                    # if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        print('attachment')
+                        # TODO: only entertain richlink conversions
+                        print(message['message'].get('attachments'))
+                        # response_sent_nontext = get_message()
+                        # send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+
+def pushVideoClip(recipient_id, url):
+    send_message(recipient_id, 'We are processing your request')
+    if(generateVideoClip(url) == 1):
+        send_video(recipient_id, os.getcwd() + '/final.mp4')
+    else:
+        send_message(recipient_id, 'Your request could not be processed, please ensure you\'ve entered a valid url')
+
+
+def verify_fb_token(token_sent):
+    """ take token sent by facebook and verify it matches the verify token you sent
+        if they match, allow the request, else return an error"""
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+
+def get_message():
+    """chooses a random message to send to the user"""
+    sample_responses = ["You are stunning!", "We're proud of you.",
+                        "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+
+def send_message(recipient_id, response):
+    """sends user the text message provided via `response` parameter"""
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+
+def send_video(recipient_id, video_path):
+    """sends user the video file provided via `video_path` parameter"""
+    bot.send_video(recipient_id, video_path)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ generate.py
@@ -0,0 +1,105 @@
+from newspaper import Article
+import urllib.request
+import numpy as np
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from moviepy.editor import *
+from moviepy.video.tools.drawing import color_gradient
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import random
+
+ARTICLE_URL = "https://www.androidpolice.com/2020/08/17/facebook-starts-integrating-messenger-with-instagram/"
+N_TEXT = 3
+WIDTH, HEIGHT = 1280, 720
+INTRO_TIMEOUT = 2
+TEXT_TIMEOUT = 4
+FADE_TIMEOUT = 1
+
+
+def download_web_image(url, i):
+    path = os.getcwd() + "/images/" + str(i) + ".jpg"
+    urllib.request.urlretrieve(url, path)
+
+
+def get_music_path(lines):
+    dic = [0, 1, 2]
+    type = {0: "neg", 1: "neu", 2: "pos"}
+    length = len(lines)
+    sid = SentimentIntensityAnalyzer()
+    mood_coeff = [0, 0, 0]
+    for sentence in lines:
+        ma = -1
+        ss = sid.polarity_scores(sentence)
+        for k in ss:
+            temp = int(ss[k] * 1000)
+            j = 0
+            if(k == "neg"):
+                j = 0
+            elif(k == "neu"):
+                j = 1
+            elif(k == "pos"):
+                j = 2
+            mood_coeff[j] += temp
+    mizaz = 1  # default neutral mood
+    total_max = 0
+    for i in range(3):
+        mood_coeff[i] /= length
+        if(mood_coeff[i] > total_max):
+            mizaz = i
+    r = random.randint(0, 1)
+    music_string = "sound_tracks/" + type[mizaz] + "-" + str(r) + ".mp3"
+    return music_string
+
+
+def getArticleText(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    print(article.title)
+    print(article.top_image)
+    lines = article.text.split('\n')
+    lines = [line for line in lines if line]
+    text = []
+    for line in lines:
+        for oneline in line.split('. '):
+            text.append(oneline)
+    text = [line for line in text if line]
+    for line in text:
+        print(line, '\n')
+    return text
+
+
+def getTextClip(text):
+    total_duration = 0
+    textClip = []
+    for line in text[0:N_TEXT]:
+        textClip.append(TextClip(line, size=(WIDTH, HEIGHT), fontsize=50,
+                                 font='AvantGarde-Book', color='black', stroke_width=5,
+                                 align='center', method='caption', kerning=1)
+                        .set_duration(TEXT_TIMEOUT).on_color(color=(255, 255, 255), col_opacity=0.5)
+                        .fadein(FADE_TIMEOUT).fadeout(FADE_TIMEOUT))
+        total_duration += TEXT_TIMEOUT
+    return concatenate(textClip, method='chain'), total_duration
+
+
+def generateVideoClip(url):
+    generated = 0
+    try:
+        text = getArticleText(url)
+        textClip, total_duration = getTextClip(text)
+        # append intro : dont concat image with text, causes pixelation
+        introClip = ImageClip('assets/newzery.png').resize(width=WIDTH, height=HEIGHT).set_pos(
+            ('center', 'center')).set_duration(total_duration + INTRO_TIMEOUT).fadein(FADE_TIMEOUT)
+        finalClip = CompositeVideoClip([introClip, textClip.set_start(
+            INTRO_TIMEOUT).set_position('center', 'center')]).set_duration(total_duration + INTRO_TIMEOUT)  # .set_audio(AudioFileClip(get_music_path(text)))
+        finalClip.write_videofile('final.mp4', fps=30, codec='mpeg4')
+        generated = 1
+    except Exception as e:
+        print(e)
+    return generated
+
+
+if __name__ == "__main__":
+    generateVideoClip(ARTICLE_URL)
